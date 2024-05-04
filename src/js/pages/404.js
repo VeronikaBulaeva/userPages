ad080dc548c60edcd75bee86bc1b5cf4acca4759
Veronika Bulaeva <veronikabylaeva99@gmail.com>
2024-05-04 11:52:33 +0300

    adding dist

--- src/js/pages/404.js
+++
@@ -1,7 +0,0 @@
-export default () => `<div class="doc-wrapper">
-  <section class="row main">
-    <div class="col-auto container">
-      <h1 class="h1 align-self-center">Страница не найдена</h1>
-    </div>
-  </section>
-</div>`
